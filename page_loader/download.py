dc8cc95854bf3d407216634710e4182fcf5efeb3
Николай Ануфриев <sch-nikolaj@yandex.ru>
2022-02-01 02:40:38 +0300

    Доработка для hexlet_check

--- page_loader/download.py
+++ page_loader/download.py
@@ -281,10 +281,10 @@ def download(url, directory):  # noqa: WPS210, C901, WPS213
     for elem in list_res:
         elem['link'].scheme = scheme
         elem['link'].netloc = netloc
-        if elem['link'].path[0] == '/':
-            elem['link'].path = f"{path}{elem['link'].path}"
-        else:
-            elem['link'].path = f"{path}/{elem['link'].path}"
+        # if elem['link'].path[0] == '/':
+        #     elem['link'].path = f"{path}{elem['link'].path}"
+        # else:
+        #     elem['link'].path = f"{elem['link'].path}"
 
     logger.debug(f'Список после реобразования ссылок {list_res}')
 
