4f1061ef633db7de88a888977c5fe0ace622b34b
Николай Ануфриев <sch-nikolaj@yandex.ru>
2022-02-01 03:06:55 +0300

    Доработка для hexlet_check

--- page_loader/download.py
+++ page_loader/download.py
@@ -213,11 +213,12 @@ def save_resources(list_res, directory):  # noqa: WPS210
         file_name = f'{link_to_filename(file_name)}.{ext}'
 
         file_name = join(directory, file_name)
+        link_res = join(directory.split('/')[-1], file_name)
         save_file(file_name, res, element['mode'])
         if element['obj'].get('href'):
-            element['obj']['href'] = file_name
+            element['obj']['href'] = link_res
         else:
-            element['obj']['src'] = file_name
+            element['obj']['src'] = link_res
         logger.info(f'Ресурс {file_name} сохранен.')
         progress_bar.next()  # noqa: B305
 
