c6ec565d1164afb92f81667670776507b3d92793
Николай Ануфриев <sch-nikolaj@yandex.ru>
2022-02-07 14:09:03 +0300

    Исправление замечаний к проекту

--- tests/test_page_download.py
+++ tests/test_page_download.py
@@ -8,6 +8,29 @@ import requests_mock
 from page_loader import download
 from page_loader.html_processing import link_to_filename
 
+correct_file_name = 'www-site-name-com.html'
+correct_dir_name = 'www-site-name-com_files'
+addres_page = 'https://www.site_name.com'
+addres_img1 = f'{addres_page}/img/python.jpeg'
+addres_img2 = f'{addres_page}/img/python_real.svg'
+addres_style = f'{addres_page}/files/css/style.css'
+addres_js = f'{addres_page}/empty.js'
+file_name = 'tests/fixtures/very_long_and_complicated_site_name.html'
+
+with open(file_name, 'r') as test_page:
+    text_html = test_page.read()
+
+
+def mock_page_download(dir_path):
+    with requests_mock.Mocker() as mock:
+        mock.get(addres_page, text=text_html)
+        mock.get(addres_img1, content=b'img1_content')
+        mock.get(addres_img2, content=b'img2_content')
+        mock.get(addres_style, text='css_content')
+        mock.get(addres_js, text='js_content')
+        received_patch = download(addres_page, dir_path)
+    return received_patch
+
 
 def test_page_loader():  # noqa: WPS210
     """
@@ -20,102 +43,50 @@ def test_page_loader():  # noqa: WPS210
     странице.
     """
     indicator = False
-    correct_file_name = 'www-site-name-com.html'
-    correct_dir_name = 'www-site-name-com_files'
-    addres_page = 'https://www.site_name.com'
-    addres_img1 = f'{addres_page}/img/python.jpeg'
-    addres_img2 = f'{addres_page}/img/python_real.svg'
-    addres_style = f'{addres_page}/files/css/style.css'
-    addres_js = f'{addres_page}/empty.js'
-    file_name = 'tests/fixtures/very_long_and_complicated_site_name.html'
-
-    with open(file_name, 'r') as test_page:
-        text_html = test_page.read()
 
     with TemporaryDirectory() as temp_dir:
+        # Ожидаемый путь
         expected_path = join(temp_dir, correct_file_name)
-        with requests_mock.Mocker() as mock:
-            mock.get(addres_page, text=text_html)
-            mock.get(addres_img1, content=b'img1_content')
-            mock.get(addres_img2, content=b'img2_content')
-            mock.get(addres_style, text='css_content')
-            mock.get(addres_js, text='js_content')
-            received_patch = download(addres_page, temp_dir)
+        # Получаем путь
+        received_patch = mock_page_download(temp_dir)
+        # существует ли файл хтмл и папка с ресурсами,
+        # название папки тожее должно быть правильным
         if exists(received_patch) and exists(join(temp_dir, correct_dir_name)):
             indicator = True
 
+        # список файлов в ожидаемой папке с ресурсами
         list_file = listdir(join(temp_dir, correct_dir_name))
 
+    # имя скачанного хтмл-файла
     current_file_name = received_patch.split('/')[-1]
 
-    assert expected_path == received_patch
-    assert indicator
-    assert correct_file_name == current_file_name
-    assert len(list_file) == 4
+    assert expected_path == received_patch  # Проверка на соответствие пути
+    assert indicator  # Проверка наличия файлов
+    assert correct_file_name == current_file_name   # Проверка на имени файла
+    assert len(list_file) == 4   # Количество скаченных фалов
 
 
-# def test_download():  # noqa: WPS210
-#     """
-#     Тест:
-#         - функции download, подача несуществующей папки
-#          и неправильной ссылки тестируется в test_link.
-#     """
-#     addres_page = 'https://www.site_name.com'
-#     addres_img1 = f'{addres_page}/img/python.jpeg'
-#     addres_img2 = f'{addres_page}/img/python_real.svg'
-#     addres_style = f'{addres_page}/files/css/style.css'
-#     addres_js = f'{addres_page}/empty.js'
-#
-#     with TemporaryDirectory() as temp_dir:
-#         with requests_mock.Mocker() as mock:
-#             mock.get(addres_page, text='html_content')
-#             mock.get(addres_img1, content=b'img1_content')
-#             mock.get(addres_img2, content=b'img2_content')
-#             mock.get(addres_style, text='css_content')
-#             mock.get(addres_js, text='js_content')
-#             received_patch = download(addres_page, temp_dir)
-#
-#
-#     assert len(list_file) == 4
-
-
-def test_link():  # noqa: WPS210
+def test_html():  # noqa: WPS210
     """
     Тест:
         - проверка правильности ссылки на ресурс внутри страницы,
     """
-    addres_page = 'https://www.site_name.com'
-    addres_img1 = f'{addres_page}/img/python.jpeg'
-    addres_img2 = f'{addres_page}/img/python_real.svg'
-    addres_css = f'{addres_page}/files/css/style.css'
-    addres_js = f'{addres_page}/empty.js'
-    file_name = 'tests/fixtures/very_long_and_complicated_site_name.html'
-    list_link = [
-        'www-site-name-com_files/www-site-name-com-files-css-style.css',
-        'www-site-name-com_files/www-site-name-com-img-python-real.svg',
-        'www-site-name-com_files/www-site-name-com-img-python.jpeg',
-        'www-site-name-com_files/www-site-name-com-empty.js',
-    ]
 
-    with open(file_name, 'r') as test_page:
-        text_html = test_page.read()
+    with open('tests/fixtures/expected.html', 'r') as test_file:
+        expected = test_file.read()
 
     with TemporaryDirectory() as temp_dir:
-        with requests_mock.Mocker() as mock:
-            mock.get(addres_page, text=text_html)
-            mock.get(addres_img1, content=b'img1_content')
-            mock.get(addres_img2, content=b'img2_content')
-            mock.get(addres_css, text='css_content')
-            mock.get(addres_js, text='js_content')
-            received_patch = download(addres_page, temp_dir)
+        # Получаем путь
+        received_patch = mock_page_download(temp_dir)
+        # получаем содержимое полученного хтмл файла
         with open(received_patch, 'r') as file_html:
             received = file_html.read()
-        expected = [True if elem in received else False for elem in list_link]
 
-    assert all(expected), received
+    assert expected == received  # Сравнение хтмл-файлов
 
 
 def test_exception():
+    """Тестирование исключений."""
     with TemporaryDirectory() as temp_dir:
         with pytest.raises(FileNotFoundError):
             download('Непонятный_сайт.', 'Непонятная_папка')
